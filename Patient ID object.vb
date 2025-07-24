1512cc914a17192b0a019d93698d44e145b6419e
Mohamed Saddami <jdmed2913@gmail.com>
2025-07-24 15:13:16 +0200

    app modified

---
+++ Patient ID object.vb
@@ -0,0 +1,14 @@
+Patient ID                                                object
+Patient age quantile                                       int64
+SARS-Cov-2 exam result                                    object
+Patient addmited to regular ward (1=yes, 0=no)             int64
+Patient addmited to semi-intensive unit (1=yes, 0=no)      int64
+                                                          ...   
+HCO3 (arterial blood gas analysis)                       float64
+pO2 (arterial blood gas analysis)                        float64
+Arteiral Fio2                                            float64
+Phosphor                                                 float64
+ctO2 (arterial blood gas analysis)                       float64
+Length: 111, dtype: object
+
+[('pipeline', ...), ('adaboostclassifier', ...)]
